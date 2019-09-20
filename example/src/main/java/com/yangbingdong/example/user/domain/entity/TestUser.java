52d05bf577f804bddc2c9a7a8b7c3a8c7defe320
shuleisanshi <shuleisanshi@163.com>
2019-09-20 12:23:59 +0800

    :zap: :sparkles: multi expire

--- example/src/main/java/com/yangbingdong/example/user/domain/entity/TestUser.java
+++ example/src/main/java/com/yangbingdong/example/user/domain/entity/TestUser.java
@@ -66,7 +66,7 @@ public class TestUser implements Serializable {
     public static final String DELETED = "deleted";
 
     public static final String TEST_UK = "test_uk";
-    /* !!TUPLE_MARK_END!!! */
+    /* !!!TUPLE_MARK_END!!! */
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
